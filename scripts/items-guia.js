f3970926f73ca133de5b2b529374d9a6dacfb5fc
Benjamin Smith <bsmith@scnctech.com>
2025-06-25 18:15:48 -0700

    más info, primera versión de negocios

--- scripts/items-guia.js
+++ scripts/items-guia.js
@@ -1,43 +1,408 @@
 // Business data
 const businessData = {
   "businesses": [
+    // ONLINE: PAMPA DIRECT
     {
       "general": {
-        "nombre": "Restaurante El Buen Sabor",
-        "titulo": "Cocina Tradicional Mexicana",
-        "descripcion": "Auténtica comida mexicana preparada con ingredientes frescos y recetas tradicionales familiares. Ambiente acogedor y servicio excepcional."
+        "nombre": "PAMPA DIRECT",
+        "titulo": "Productos Argentinos y Uruguayos",
+        "descripcion": "Alfajores, yerba, tapas de empanadas, indumentaria y más. Envio gratis con una compra de más de $50."
       },
       "organizacionEnGuia": {
-        "categoria": "Restaurantes",
-        "subcategoria": "Comida Mexicana"
+        "categoria": "Online",
+        "subcategoria": "Productos Argentinos y Uruguayos"
       },
       "ubi": {
-        "direccion": "Av. Revolución 1234, Col. Centro, Ciudad de México",
-        "horario": [
-          {"dia": "Lunes", "apertura": "09:00", "cierre": "22:00"},
-          {"dia": "Martes", "apertura": "09:00", "cierre": "22:00"},
-          {"dia": "Miércoles", "apertura": "09:00", "cierre": "22:00"},
-          {"dia": "Jueves", "apertura": "09:00", "cierre": "22:00"},
-          {"dia": "Viernes", "apertura": "09:00", "cierre": "23:00"},
-          {"dia": "Sábado", "apertura": "Cerrado", "cierre": "Cerrado"},
-          {"dia": "Domingo", "apertura": "Cerrado", "cierre": "Cerrado"}
-        ]
+        "direccion": "",
+        "horario": []
       },
       "etc": {
-        "urlImagen": "https://images.unsplash.com/photo-1514933651103-005eec06c04b?w=400"
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [],
+        "email": ""
+      },
+      "urls": []
+    },
+    // WOODEN TABLE CO
+    {
+      "general": {
+        "nombre": "WOODEN TABLE CO",
+        "titulo": "",
+        "descripcion": ""
+      },
+      "organizacionEnGuia": {
+        "categoria": "Negocios",
+        "subcategoria": ""
+      },
+      "ubi": {
+        "direccion": "",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [],
+        "email": ""
+      },
+      "urls": []
+    },
+    // TASTE ARGENTO
+    {
+      "general": {
+        "nombre": "TASTE ARGENTO",
+        "titulo": "",
+        "descripcion": "Alfajores: Aguila, Havanna, Cachafaz, Milka, Rasta, Terrabusi, Jorgito; Dulce de leche clásico y colonial. Yerba, Mate cocido, polenta, dulce de batata y membrillo, Titas, Rodhesias, Chocolatinas, Rocklets, Savora, Mayonesa Natura, Don Satur, Surtidos Bagley, pico dulce, operas y mucho más!"
+      },
+      "organizacionEnGuia": {
+        "categoria": "Negocios",
+        "subcategoria": "Tienda de productos argentinos"
+      },
+      "ubi": {
+        "direccion": "22 NE 2nd Ave, Portland",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
       },
       "contacto": {
         "telefonos": [
-          {"tipo": "Principal", "numero": "555-123-4567", "extension": ""},
-          {"tipo": "WhatsApp", "numero": "555-123-4568", "extension": ""}
+          {"tipo": "Principal", "numero": "760-896-9716", "extension": ""},
+          {"tipo": "Alternativo", "numero": "971-449-5737", "extension": ""}
         ],
-        "email": "info@elbuensabor.com"
+        "email": ""
+      },
+      "urls": []
+    },
+    // EL CAMPESINO PDX
+    {
+      "general": {
+        "nombre": "EL CAMPESINO PDX",
+        "titulo": "",
+        "descripcion": "Yerba, mates, dulce de leche, tapas de empanadas, alfajores y más por pedido. A veces tienen mollejas."
+      },
+      "organizacionEnGuia": {
+        "categoria": "Negocios",
+        "subcategoria": "Tienda de productos argentinos"
+      },
+      "ubi": {
+        "direccion": "6611 SE Powell Blvd, Portland OR 97206",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [
+          {"tipo": "Principal", "numero": "503-774-4124", "extension": ""}
+        ],
+        "email": ""
+      },
+      "urls": []
+    },
+    // KAHN MARKET
+    {
+      "general": {
+        "nombre": "KAHN MARKET",
+        "titulo": "",
+        "descripcion": "Alfajores Havana y Cachafaz, galletitas Cachafaz, Bon o Bon, mantecol, Yerba Taragui, Rosamonte, Canarias, Amanda, Union, Mate cocido varios, Playadito, Cachamate (varias hierbas), dulce de leche San Ignacio/Serenísima, Bizcochitos Don Satur y 9 de oro."
+      },
+      "organizacionEnGuia": {
+        "categoria": "Negocios",
+        "subcategoria": "Tienda de productos argentinos"
+      },
+      "ubi": {
+        "direccion": "6927 SE Foster Rd, Portland",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [],
+        "email": ""
+      },
+      "urls": []
+    },
+    // VILLAS MARKET
+    {
+      "general": {
+        "nombre": "VILLAS MARKET",
+        "titulo": "",
+        "descripcion": "Milanesas de carne y de pollo, yerba, dulce de leche, tapas de empanadas, Cafe La Virginia, Sal Dos Anclas, alfajores, Don Satur y Pepas"
+      },
+      "organizacionEnGuia": {
+        "categoria": "Negocios",
+        "subcategoria": "Tienda de productos argentinos"
+      },
+      "ubi": {
+        "direccion": "9515 SE 82nd Ave, Happy Valley OR 97086 (Tambien aparece como Portland en Google)",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [
+          {"tipo": "Principal", "numero": "503-719-6047", "extension": ""}
+        ],
+        "email": ""
+      },
+      "urls": []
+    },
+    // TIENDA NOHBEC
+    {
+      "general": {
+        "nombre": "TIENDA NOHBEC",
+        "titulo": "",
+        "descripcion": "Tapas de empanadas, dulce de batata y membrillo, dulce de leche, alfajores, sidra, turrones, chorizo colorado"
+      },
+      "organizacionEnGuia": {
+        "categoria": "Negocios",
+        "subcategoria": "Tienda de productos argentinos"
+      },
+      "ubi": {
+        "direccion": "18929 E Burnside St, Portland OR 97233",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [
+          {"tipo": "Principal", "numero": "503-667-4444", "extension": ""}
+        ],
+        "email": ""
+      },
+      "urls": []
+    },
+    // BECERRAS INTERNATIONAL GROCERIES
+    {
+      "general": {
+        "nombre": "BECERRAS INTERNATIONAL GROCERIES",
+        "titulo": "",
+        "descripcion": ""
+      },
+      "organizacionEnGuia": {
+        "categoria": "Negocios",
+        "subcategoria": ""
+      },
+      "ubi": {
+        "direccion": "3503 E 4th Blvd, Vancouver WA",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [
+          {"tipo": "Principal", "numero": "360-690-0044", "extension": ""}
+        ],
+        "email": ""
+      },
+      "urls": []
+    },
+    // BARBUR WORLD FOODS
+    {
+      "general": {
+        "nombre": "BARBUR WORLD FOODS",
+        "titulo": "",
+        "descripcion": "A veces tapas de empanadas, tarta"
+      },
+      "organizacionEnGuia": {
+        "categoria": "Negocios",
+        "subcategoria": ""
+      },
+      "ubi": {
+        "direccion": "9845 SW Barbur Blvd, Portland OR 97219",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [
+          {"tipo": "Principal", "numero": "503-244-0670", "extension": ""}
+        ],
+        "email": ""
+      },
+      "urls": []
+    },
+    // WHOLE FOODS
+    {
+      "general": {
+        "nombre": "WHOLE FOODS",
+        "titulo": "",
+        "descripcion": "Tiene alfajores de maizena chicos a $2 cada uno en la sección fresh cookies"
+      },
+      "organizacionEnGuia": {
+        "categoria": "Negocios",
+        "subcategoria": ""
+      },
+      "ubi": {
+        "direccion": "",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [],
+        "email": ""
+      },
+      "urls": []
+    },
+    // FACTURAS (por encargue)
+    {
+      "general": {
+        "nombre": "FACTURAS (por encargue)",
+        "titulo": "",
+        "descripcion": "Portland Contactar a Lari por WhatsApp +54 9 3813 01 6272, Seattle: https://www.facebook.com/SeaTangoArg/"
+      },
+      "organizacionEnGuia": {
+        "categoria": "Productos",
+        "subcategoria": "Facturas"
+      },
+      "ubi": {
+        "direccion": "",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [
+          {"tipo": "WhatsApp", "numero": "+54 9 3813 01 6272", "extension": ""}
+        ],
+        "email": ""
       },
       "urls": [
-        {"descripcion": "Sitio Web", "url": "https://www.elbuensabor.com"},
-        {"descripcion": "Facebook", "url": "https://facebook.com/elbuensabor"}
+        {"descripcion": "Facebook", "url": "https://www.facebook.com/SeaTangoArg/"}
       ]
     },
-    
+    // CARNE PARA MILANESA (varios supermercados)
+    {
+      "general": {
+        "nombre": "CARNE PARA MILANESA (varios supermercados)",
+        "titulo": "",
+        "descripcion": "WALMART: corte 'milanesa' (thin milanesa cut). SAFEWAY: 'beef eye of round steak thin'. NEW SEASONS: milanesas de pollo ya rebozadas 'breaded chicken'"
+      },
+      "organizacionEnGuia": {
+        "categoria": "Productos",
+        "subcategoria": "Carne para milanesa"
+      },
+      "ubi": {
+        "direccion": "",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [],
+        "email": ""
+      },
+      "urls": []
+    },
+    // LENGUA, MOLLEJA, CHINCHULINES
+    {
+      "general": {
+        "nombre": "LENGUA, MOLLEJA, CHINCHULINES",
+        "titulo": "",
+        "descripcion": "Pedir en carnicerias Mexicanas. Se ha visto en SU CASA SUPERMERCADO 1050 SE Walnut St, Hillsboro, OR 97123 y SUPERMERCADOS 970 SE Oak St, Hillsboro OR"
+      },
+      "organizacionEnGuia": {
+        "categoria": "Productos",
+        "subcategoria": "Carnicerías"
+      },
+      "ubi": {
+        "direccion": "SU CASA SUPERMERCADO 1050 SE Walnut St, Hillsboro, OR 97123 y SUPERMERCADOS 970 SE Oak St, Hillsboro OR",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [],
+        "email": ""
+      },
+      "urls": []
+    },
+    // MORCILLAS LAURELHURST MARKET
+    {
+      "general": {
+        "nombre": "MORCILLAS LAURELHURST MARKET",
+        "titulo": "",
+        "descripcion": "Butcher shop, morcillas"
+      },
+      "organizacionEnGuia": {
+        "categoria": "Productos",
+        "subcategoria": "Carnicerías"
+      },
+      "ubi": {
+        "direccion": "3155 E Burnside St, Portland OR 97214",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [],
+        "email": ""
+      },
+      "urls": []
+    },
+    // ONLINE: MR TANGO SAUSAGES
+    {
+      "general": {
+        "nombre": "MR TANGO SAUSAGES",
+        "titulo": "",
+        "descripcion": "Online: https://www.mrtangosausages.com/product-category/blood-sausages/"
+      },
+      "organizacionEnGuia": {
+        "categoria": "Online",
+        "subcategoria": "Carnicerías"
+      },
+      "ubi": {
+        "direccion": "",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [],
+        "email": ""
+      },
+      "urls": [
+        {"descripcion": "Sitio Web", "url": "https://www.mrtangosausages.com/product-category/blood-sausages/"}
+      ]
+    },
+    // DULCE DE LECHE
+    {
+      "general": {
+        "nombre": "DULCE DE LECHE",
+        "titulo": "",
+        "descripcion": "Amazon.com La Lechera (En Walmart-chileno), El Campesino -Marca Havana"
+      },
+      "organizacionEnGuia": {
+        "categoria": "Productos",
+        "subcategoria": "Dulce de leche"
+      },
+      "ubi": {
+        "direccion": "",
+        "horario": []
+      },
+      "etc": {
+        "urlImagen": ""
+      },
+      "contacto": {
+        "telefonos": [],
+        "email": ""
+      },
+      "urls": []
+    }
   ]
 };
