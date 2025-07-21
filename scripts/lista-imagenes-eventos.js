a36fd041808f9b54364d700c580695c882bb7290
Benjamin Smith <bsmith@scnctech.com>
2025-07-21 15:42:04 -0700

    fix paf23, agrego eventos

---
+++ scripts/lista-imagenes-eventos.js
@@ -0,0 +1,33 @@
+// Configuration: set your image directory and filenames here
+const imageDir = './imagenes/eventos/';
+const imageFiles = [
+    'img-1311_orig.jpg',
+    'img-1350_orig.jpg',
+    'img-1375_orig.jpg',
+    'img-1443_orig.jpg',
+    'p111_orig.png',
+    'p121_orig.png',
+    'p127_orig.png',
+    'img-1316_orig.jpg',
+    'img-1362_orig.jpg',
+    'img-1386_orig.jpg',
+    'img-6602_orig.jpg',
+    'p112_orig.png',
+    'p123_orig.png',
+    'p128_orig.png',
+    'img-1339_orig.jpg',
+    'img-1369_orig.jpg',
+    'img-1428_orig.jpg',
+    'p109_orig.png',
+    'p113_orig.png',
+    'p124_orig.png',
+    'trio1_orig.jpeg',
+    'img-1345_orig.jpg',
+    'img-1374_orig.jpg',
+    'img-1441_orig.jpg',
+    'p110_orig.png',
+    'p119_orig.png',
+    'p126_orig.png',
+    // Add more filenames as needed
+];
+
---
+++ scripts/lista-imagenes-paf-23.js
@@ -0,0 +1,17 @@
+// Configuration: set your image directory and filenames here
+const imageDir = './imagenes/paf/23/';
+const imageFiles = [
+    'kc4b1992_orig.jpg',
+    'paf23-221_orig.jpg',
+    'paf23-255_orig.jpg',
+    'paf23-258_orig.jpg',
+    'paf23-310_orig.jpg',
+    'paf23-315_orig.jpg',
+    'paf23-216_orig.jpg',
+    'paf23-247_orig.jpg',
+    'paf23-257_orig.jpg',
+    'paf23-306_orig.jpg',
+    'paf23-311_orig.jpg',
+    'paf23-320_orig.jpg',
+    // Add more filenames as needed
+];
