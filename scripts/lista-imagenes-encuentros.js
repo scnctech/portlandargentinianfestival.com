e5ff2b361105e10eb93c588ae953200fbb01dd5e
Benjamin Smith <bsmith@scnctech.com>
2025-09-07 08:48:19 -0700

    imagenes en encuentros mensuales

---
+++ scripts/lista-imagenes-encuentros.js
@@ -0,0 +1,18 @@
+// Configuration: set your image directory and filenames here
+const imageDir = './imagenes/encuentros/';
+const imageFiles = [
+    'agosto2024.jpg',
+    'diciembre2019-2.jpg',
+    'diciembre2019.jpg',
+    'diciembre2024.jpg',
+    'febrero2023.jpg',
+    'julio2023.jpg',
+    'julio2025.jpg',
+    'marzo2023.jpg',
+    'marzo2024.jpg',
+    'mayo2024-2.jpg',
+    'mayo2024.jpg',
+    'noviembre2023.jpg',
+    'octubre2021.jpg',
+    // Add more filenames as needed
+];
